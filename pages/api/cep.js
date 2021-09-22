cbd01e4ccf1d0c982584f18c781f084db947b429
Tiago <tiago.goes2009@gmail.com>
2021-09-22 19:58:46 -0300

    api

--- pages/api/cep.js
+++ pages/api/cep.js
@@ -1,8 +1,11 @@
-function Cep(request, response) {
-  console.log(request)
-  response.json({
-    status: "OK"
-  })
+async function Cep(request, response) {
+  const postalCode = request.query.code
+  console.log(postalCode)
+  const viaCepResponse = await fetch(`https://viacep.com.br/ws/${postalCode}/json/`)
+  const postalCodeData = await viaCepResponse.json()
+
+  response.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate')
+  response.json(postalCodeData)
 }
 
 export default Cep
